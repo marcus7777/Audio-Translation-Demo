78d933604c1790c020db12d5a1bd2b1cee14c214
marcus <marcus@hhost.me>
2019-07-10 11:16:45 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>audio</span>
+        <span class="font-weight-light">TRANSLATION DEMO</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import './plugins/vuetify'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+import AudioRecorder from 'vue-audio-recorder'
+
+Vue.use(AudioRecorder)
 
 new Vue({
   render: h => h(App),
